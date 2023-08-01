b6d8dc8dcb90636e494c822f06adf43ada5e2f1c
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-01 03:01:33 -0500

    Complete hashes_1 Part 2

--- hashes_1.rb
+++ hashes_1.rb
@@ -46,17 +46,30 @@ puts foods
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+email = {
+  sender: "Michael",
+  subject_line: "Resignation",
+  body: "To whom it may concern",
+  recipients: ["Joe", "Patrick"],
+  time_sent: "18:00:00 08/06/23", 
+  read_receipt: true
+}
+
 # Write code that prints your email hash to the terminal.
 
+puts email
 
 # Write code that prints all of the 'keys' of the email hash 
 # you created above:
 # YOUR CODE HERE
 
+puts email.keys
+
 # Write code that prints all of the 'values' of the email hash 
 # you created above:
 # YOUR CODE HERE
 
+puts email.values
 
 #-------------------
 # Part 3: Many Emails - CHALLENGE!
